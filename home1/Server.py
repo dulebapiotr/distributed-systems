db3700bdfe8bab203154713c6213406d0c3d8966
Piotr <duleba.pio@gmail.com>
2020-03-01 09:06:38 +0100

    home1 basic features works

--- home1/Server.py
+++ home1/Server.py
@@ -1,21 +1,64 @@
 #!/usr/bin/env python3
 
 import socket
-import time
+import select
+import sys
+import threading
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+if(len(sys.argv) != 2):
+    print("Put <port number> as an argument!")
+    exit()
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5013
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+IP_ADDR = '127.0.0.1'
+PORT = int(sys.argv[1])
+MAX_CLIENTS = 100
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+server.bind((IP_ADDR,PORT))
+server.listen(MAX_CLIENTS)
 
-conn, addr = s.accept()
-print ('Connection address:', addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print("received data:", data)
+#list of client sockets
+clients = []
+clients_name = []
+
+def client_connection_thread(con_socket, address, name):
+    con_socket.send("Welcome to chat!".encode())
+    while True:
+        try:
+            message = con_socket.recv(1024).decode()
+            if message:
+                print("Received message "+message)
+                message_to_send = "<"+name+"> "+message
+                broadcast(message_to_send, con_socket)
+            else:
+                remove(con_socket)    
+        except:
+            continue
+            
+
+def broadcast(message, con_socket):
+    for client in clients:
+        if client != con_socket:
+            try:
+                print("Sending message "+message)
+                client.send(message.encode())
+            except:
+                client.close()
+                remove(client)
+           
+def remove(con_socket):
+    if con_socket in clients:
+        del clients[clients.index(con_socket)]
+        clients.remove(con_socket)
+
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    name = conn.recv(1024).decode()
+    clients_name.insert(clients.index(conn), name)
+    print("Starting thread for "+name)
+    threading.Thread(target=client_connection_thread, args=(conn,addr,name)).start()
+
 conn.close()
+server.close()
