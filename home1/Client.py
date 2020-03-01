c635c57c56017743a272e8fe7a9569747a7a064d
Piotr <duleba.pio@gmail.com>
2020-03-01 12:15:40 +0100

    home1 udp channel works, some cleanup needed

--- home1/Client.py
+++ home1/Client.py
@@ -3,29 +3,53 @@
 import socket
 import sys
 import select
+import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def udp_receive_thread(udp_sck):
+    while True:
+        try:
+            buff, address = udp_sck.recvfrom(1024)
+            print(str(buff,'utf-8'))
+
+        except:
+            continue
+
+tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 if len(sys.argv) != 3:
     print("Put <port number> and <client name> as an arguments!")
     exit()
 IP_ADDR = '127.0.0.1'
 PORT = int(sys.argv[1])
 NAME = str(sys.argv[2])
-server.connect((IP_ADDR,PORT))
-server.send(NAME.encode())
 print("Hello "+NAME)
-print("Connection has been established successfully!")
 
+tcp_client.connect((IP_ADDR,PORT))
+tcp_client.send(NAME.encode())
+print("TCP Connection has been established successfully!")
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+
+threading.Thread(target=udp_receive_thread, args=((udp_socket,))).start()
+print("adr tcp: "+ str(tcp_client.getsockname()))
+udp_socket.bind((tcp_client.getsockname()))
 while True:
-    read_list=[server, sys.stdin]
+    read_list=[tcp_client, sys.stdin]
     read_ready, write_ready, err = select.select(read_list,[],[])
     for source in read_ready:
-        if source == server:
-            message = server.recv(1024)
+        if source == tcp_client:
+            message = tcp_client.recv(1024)
             print(message.decode())
-        else:
+        elif source == sys.stdin:
             message = sys.stdin.readline()
-            server.send(message.encode())
-            print("<"+NAME+"> " + message.replace('\n', ' ').replace('\r', '')) 
+            if message[0]== 'U':
+                print("my addr: "+str(udp_socket.getsockname()))
+                udp_socket.sendto(bytes("<"+NAME+">UDP "+ message[1:],'utf-8'),(IP_ADDR,PORT)) 
+            else:
+                tcp_client.send(message.encode())
+                print("<"+NAME+"> " + message.replace('\n', ' ').replace('\r', ''))
+        
 
-server.close()
+tcp_client.close()
--- home1/Server.py
+++ home1/Server.py
@@ -7,6 +7,8 @@ import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+
 if(len(sys.argv) != 2):
     print("Put <port number> as an argument!")
     exit()
@@ -15,14 +17,20 @@ IP_ADDR = '127.0.0.1'
 PORT = int(sys.argv[1])
 MAX_CLIENTS = 100
 
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind((IP_ADDR,PORT))
+
 server.bind((IP_ADDR,PORT))
 server.listen(MAX_CLIENTS)
 
 #list of client sockets
 clients = []
+clients_address=[]
 clients_name = []
 
-def client_connection_thread(con_socket, address, name):
+
+def client_connection_thread(con_socket, name):
     con_socket.send("Welcome to chat!".encode())
     while True:
         try:
@@ -50,15 +58,35 @@ def broadcast(message, con_socket):
 def remove(con_socket):
     if con_socket in clients:
         del clients[clients.index(con_socket)]
+        del clients_address[clients.index(con_socket)]
         clients.remove(con_socket)
 
+def udp_receive_thread(udp_sck):
+    while True:
+        try:
+            buff, address = udp_sck.recvfrom(1024)
+            print("Received udp: "+str(buff,'utf-8'))
+            udp_sck.sendto(bytes("MSG FROM SERVER - UDP has been received", 'utf-8'), (address) )
+            print("rcv adr: "+ str(address))
+            for adr in clients_address:
+                udp_sck.sendto(buff, (adr) )
+            print("broadcats completed")
+        except:
+            continue
+
+        
+print("udp adr: "+str(udp_socket.getsockname()))
+print("tcp adr: "+str(server.getsockname()))
+threading.Thread(target=udp_receive_thread, args=((udp_socket,))).start()
 while True:
     conn, addr = server.accept()
+    clients_address.append(addr)
     clients.append(conn)
     name = conn.recv(1024).decode()
     clients_name.insert(clients.index(conn), name)
     print("Starting thread for "+name)
-    threading.Thread(target=client_connection_thread, args=(conn,addr,name)).start()
+    print("Kasia adr: "+ str(addr))
+    threading.Thread(target=client_connection_thread, args=(conn,name)).start()
 
 conn.close()
 server.close()
